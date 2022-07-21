876a7bfbe40b2993ca68e7cf4f74f6e5d407dd12
Leticiamkabu <kabuleticia36@gmail.com>
2022-07-21 09:51:22 -0700

    a class Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,7 +25,7 @@ class Square:
             raise TypeError("size must be an integer")                                                                                          elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-                                                                                                                                            @property
+    @property
     def position(self):
         """Get/set the current position of the square."""
         return (self.__position)
